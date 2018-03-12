f09da2df41806bd0322ef7b0606af3b7d80791e5
463836190 <463836190@qq.com>
2018-03-12 16:53:14 +0800

    update

--- 6-11.py
+++ 6-11.py
@@ -1,10 +1,8 @@
 def picprint(length = 5, width = 5, pic = '*'):
     for i in range(1 , length + 1):
         for j in range(1 , width + 1):
-            if(j != width):
-                print(pic, end='')
-            else:
-                print(pic)
+            print(pic, end='')
+        print()
 
 picprint()
 picprint(4,3)
