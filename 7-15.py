e963dbcac4629ffcd0322981a12a1e3e003b4015
463836190 <463836190@qq.com>
2018-03-13 14:44:26 +0800

    update

--- 7-15.py
+++ 7-15.py
@@ -5,12 +5,33 @@ total = []
 num = 0
 while num <200:
     str = ''
-    for j in range(9):
+    for j in range(8):
         str = str + random.choice(string.ascii_letters)
     if(str not in total):
         total.append(str)
         num = num + 1
     print(num,total[num-1])
+
+#方法2
+total2 = []
+num2 = 0
+while num2 <200:
+    str = random.sample(string.ascii_letters,8)
+    if(str not in total2):
+        total2.append(str)
+        num2 = num2 + 1
+    print(num2,''.join(total2[num2-1]))
+
+#方法3
+total3 = []
+num3 = 0
+while num3 <200:
+    str = ''.join(random.shuffle(list(string.ascii_letters))[:7])
+    if(str not in total3):
+        total3.append(str)
+        num3 = num3 + 1
+    print(num3,total3[num3-1])
+
 
 
 
